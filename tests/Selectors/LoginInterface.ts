b5dc668dc879d13818028135dc481d757fb1f459
learner25 <saimon2999@gmail.com>
2024-12-13 05:31:51 +0600

    minor changes

--- tests/Selectors/LoginInterface.ts
+++ tests/Selectors/LoginInterface.ts
@@ -2,12 +2,12 @@
 export interface LoginInterface {
     readonly EMAIL: string;
     readonly PASSWORD: string;
-    readonly VERSION: string;
+    
   }
   
   export const LoginSelectors: LoginInterface = {
     EMAIL: "form input[type='email']",
     PASSWORD: "form input[type='password']",
-    VERSION: "1.0.0",
+   
   };
   
