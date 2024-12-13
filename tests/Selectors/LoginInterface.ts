5f4fe5ab3325ad36b95de17e78e793d234be6399
learner25 <saimon2999@gmail.com>
2024-12-13 07:16:31 +0600

    add some locators

--- tests/Selectors/LoginInterface.ts
+++ tests/Selectors/LoginInterface.ts
@@ -2,12 +2,15 @@
 export interface LoginInterface {
     readonly EMAIL: string;
     readonly PASSWORD: string;
+    readonly LOGINEMAIL:string;
+    readonly LOGINPASSBUTTON: string;
     
   }
   
   export const LoginSelectors: LoginInterface = {
     EMAIL: "form input[type='email']",
     PASSWORD: "form input[type='password']",
-   
+    LOGINEMAIL:".LoginEmailForm-continueButton",
+    LOGINPASSBUTTON:".LoginPasswordForm-loginButton"
   };
   
